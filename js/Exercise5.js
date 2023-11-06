1d1c4c4595ea5b42afb3059e0ab7c011c3cdb4fe
Radyan Rahmananta <radyanrahmananta23@gmail.com>
2023-11-06 16:15:45 +0700

    Exercise 5

---
+++ js/Exercise5.js
@@ -0,0 +1,7 @@
+myColor = ['Red', 'Green', 'White', 'Black']
+
+let color = myColor.join()
+let color2 = myColor.join('+')
+
+console.log(color)
+console.log(color2)
--- js/Loop.js
+++ js/Loop.js
@@ -17,3 +17,20 @@ while (iterator < dogs.length) {
   console.log(dogs[iterator])
   iterator++
 }
+
+dogs.forEach(dog => {
+  console.log(dog)
+})
+
+var manyDogs = dogs.map(dog => {
+  return dog + 's'
+})
+console.log(manyDogs)
+
+var numbers = [1, 2, 3, 4]
+var numberMultiplied = numbers.map(number => {
+  var newNumber = number * number
+  return newNumber
+})
+
+console.log(numberMultiplied)
